e2932940014f7011e45cb5d1ff7fa018d633e1a8
davidwah <davidwahyuyu@gmail.com>
2022-07-05 01:58:25 +0700

    init project golang-gcs

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	projectID  = "project-id"
+	bucketName = "bucket-name"
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+var uploader *ClientUploader
+
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "")
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "event/",
+	}
+
+}
+
+func main() {
+	r := gin.Default()
+	//r.SetTrustedProxies([]string{"192.168.12.13:8081"})
+	r.POST("/upload", func(c *gin.Context) {
+		f, err := c.FormFile("file_input")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		blobFile, err := f.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		err = uploader.UploadFile(blobFile, f.Filename)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
